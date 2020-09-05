f00188594bc499d766df57f0fe082b3dfa0d15d9
Richie Bendall <richiebendall@gmail.com>
2020-09-05 23:30:04 +1200

    Simplify implementation

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 'use strict';
 const stringWidth = require('string-width');
 
-const widestLine = input => {
-	let max = 0;
-
-	for (const line of input.split('\n')) {
-		max = Math.max(max, stringWidth(line));
-	}
-
-	return max;
-};
+const widestLine = input => Math.max(0, ...input.split("\n").map(line => stringWidth(line)));
 
 module.exports = widestLine;
 // TODO: remove this in the next major version
