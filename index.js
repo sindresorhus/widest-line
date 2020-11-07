d1291eaf159212d8deb5c2acc999f9954cdebbcd
Richie Bendall <richiebendall@gmail.com>
2020-11-07 18:36:29 +1300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 const stringWidth = require('string-width');
 
-const widestLine = input => Math.max(0, ...input.split('\n').map(line => stringWidth(line)))
+const widestLine = input => Math.max(0, ...input.split('\n').map(line => stringWidth(line)));
 
 module.exports = widestLine;
 // TODO: remove this in the next major version
